e32ac0a84afb505ff8df4ba852c83635cd255cc0
kelly-Moon <kellymoon220@gmail.com>
2025-09-17 17:42:08 +0900

    추천 개발자 작업

--- src/components/contents/Developer.jsx
+++ src/components/contents/Developer.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { developerText } from "../../data/developer";
+import { Link } from "react-router-dom";
 
 const Developer = () => {
-  return <div>Developer</div>;
+  return (
+    <section id="developer">
+      <h2>😎추천 개발자를 소개합니다.</h2>
+      <div className="developer__inner overflow">
+        {developerText.map((develpoer, key) => (
+          <div className="developer" key={key}>
+            <div className="develpoer__img play__icon">
+              <Link to={`/channel/${develpoer.channelId}`}>
+                <img src={develpoer.img} alt={develpoer.name} />
+              </Link>
+            </div>
+            <div className="develpoer__info">
+              <Link to={`/channel/${develpoer.channelId}`}>
+                {develpoer.name}
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Developer;
--- src/pages/Developer.jsx
+++ src/pages/Developer.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import Main from "../components/section/Main";
+import { developerText } from "../data/developer";
+import { Link } from "react-router-dom";
 
 const Developer = () => {
   return (
     <Main title="추천 개발자" description="오늘의 추천 개발자 유튜버입니다.">
-      Developer
+      <section id="developerPage">
+        <h2>😎추천 개발자를 소개합니다.</h2>
+        <div className="developer__inner">
+          {developerText.map((develpoer, key) => (
+            <div className="developer" key={key}>
+              <div className="develpoer__img play__icon">
+                <Link to={`/channel/${develpoer.channelId}`}>
+                  <img src={develpoer.img} alt={develpoer.name} />
+                </Link>
+              </div>
+              <div className="develpoer__info">
+                <Link to={`/channel/${develpoer.channelId}`}>
+                  {develpoer.name}
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     </Main>
   );
 };
