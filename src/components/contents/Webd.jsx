bbb9a7a5ffe42244adaf339a000cd129dff38c02
kelly-Moon <kellymoon220@gmail.com>
2025-09-17 21:52:01 +0900

    메인 섹션 나머지 콘텐츠 작업

--- src/components/contents/Webd.jsx
+++ src/components/contents/Webd.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { webdText } from "../../data/webd";
+import { Link } from "react-router-dom";
 
 const Webd = () => {
-  return <div>Webd</div>;
+  return (
+    <section id="webd">
+      <h2>👼 웹디자인 기능사 준비는 이걸로!</h2>
+      <div className="video__inner">
+        {webdText.map((video, key) => (
+          <div className="video" key={key}>
+            <div className="video__thum play__icon">
+              <Link to={`/video/${video.videoId}`}>
+                <img src={video.img} alt={video.title} />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Webd;
--- src/components/contents/Website.jsx
+++ src/components/contents/Website.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { websiteText } from "../../data/website";
+import { Link } from "react-router-dom";
 
 const Website = () => {
-  return <div>Website</div>;
+  return (
+    <section id="website">
+      <h2>😁 웹사이트 만들기 기초 다지기</h2>
+      <div className="video__inner">
+        {websiteText.map((video, key) => (
+          <div className="video" key={key}>
+            <div className="video__thum play__icon">
+              <Link to={`/video/${video.videoId}`}>
+                <img src={video.img} alt={video.title} />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Website;
